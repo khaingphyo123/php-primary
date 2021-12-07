14bcca9487dbe7f61f95e17caca38306c0eceea9
khaingphyo123 <khaingphyo534@gmail.com>
2021-12-07 15:14:56 +0630

    add one button

--- index.php
+++ index.php
@@ -13,5 +13,6 @@ include 'include/header.php' ?>
     echo '<h1>My age is: '.$age.'</h1>';
     echo "<h1>My name is: $name </h1>";
     ?>
+    <button type="button" class="btn btn-dark">Click Me</button>
     <?php require 'include/footer.php' ?>
     
